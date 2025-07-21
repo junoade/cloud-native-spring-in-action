fc655f9f290db64b18320226caf081801b7744e3
junoade <ajchoi0928@gmail.com>
2025-07-21 23:48:03 +0900

    db: test db schema init

---
+++ Chapter05/05-begin/catalog-service/src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS book;
+
+CREATE TABLE book (
+    id          BIGSERIAL PRIMARY KEY NOT NULL,
+    author      VARCHAR(255) NOT NULL,
+    isbn        VARCHAR(255) NOT NULL,
+    price       float8 NOT NULL,
+    title       VARCHAR(255) NOT NULL,
+    version     integer NOT NULL
+);
