4c31b11ba5c3ad40e7ebc6af94b449bfa7bf9910
junoade <ajchoi0928@gmail.com>
2025-07-22 23:43:41 +0900

    refactor: Repository

--- Chapter05/05-begin/catalog-service/src/main/resources/schema.sql
+++ Chapter05/05-begin/catalog-service/src/main/resources/schema.sql
@@ -6,5 +6,7 @@ CREATE TABLE book (
     isbn        VARCHAR(255) NOT NULL,
     price       float8 NOT NULL,
     title       VARCHAR(255) NOT NULL,
+    created_date timestamp NOT NULL,
+    last_modified_date timestamp NOT NULL,
     version     integer NOT NULL
 );
