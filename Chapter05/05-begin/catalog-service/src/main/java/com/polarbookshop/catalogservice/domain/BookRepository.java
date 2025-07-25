de005c70c956f392bff072e5ab7228cce96a1f31
junoade <ajchoi0928@gmail.com>
2025-07-25 21:48:51 +0900

    test: 테스트코드 추가

--- Chapter05/05-begin/catalog-service/src/main/java/com/polarbookshop/catalogservice/domain/BookRepository.java
+++ Chapter05/05-begin/catalog-service/src/main/java/com/polarbookshop/catalogservice/domain/BookRepository.java
@@ -3,6 +3,7 @@ package com.polarbookshop.catalogservice.domain;
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -15,6 +16,6 @@ public interface BookRepository extends CrudRepository<Book, Long> {
 	@Modifying
 	@Transactional
 	@Query("DELETE FROM Book b where isbn = :isbn")
-	void deleteByIsbn(String isbn);
+	void deleteByIsbn(@Param("isbn") String isbn);
 
 }
